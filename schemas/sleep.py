ca8c0017a9e455232f8327c138e006480ea3a545
Asif Laldin <laldin.asif@gmail.com>
2025-04-26 23:03:46 +0100

    created initial sleep schema

---
+++ schemas/sleep.py
@@ -0,0 +1,29 @@
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+
+class SleepSchema(BaseModel):
+
+    start : datetime
+    end : datetime
+
+    respiratory_rate :float
+    sleep_performance_percentage:float
+    sleep_consistency_percentage:float
+    sleep_efficiency_percentage :float
+
+    # --- Stage summary fields ---
+    # total_time_in_bed_time_milli = Column(Integer)
+    # total_awake_time_milli = Column(Integer)
+    # total_no_data_time_milli = Column(Integer)
+    # total_slow_wave_sleep_time_milli = Column(Integer)
+    # total_rem_sleep_time_milli = Column(Integer)
+    sleep_cycle_count:int
+    disturbance_count:int
+
+    # --- Sleep needed fields ---
+    # baseline_sleep_needed_milli = Column(Integer)
+    # need_from_sleep_debt_milli = Column(Integer)
+    # need_from_recent_strain_milli = Column(Integer)
+    # need_from_recent_nap_milli = Column(Integer)
