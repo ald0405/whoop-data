ab139db33f08738d215642d3e9e34a950c048401
Asif Laldin <laldin.asif@gmail.com>
2025-04-26 23:05:11 +0100

    included sleep router

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from api.recovery_routes import router as recovery_router
 from api.workout_routes import router as workout_router
-from fastapi import FastAPI 
+from api.sleep_routes import router as sleep_router
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # <-- for dev, allow everything
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(recovery_router)
 app.include_router(workout_router)
+app.include_router(sleep_router)
