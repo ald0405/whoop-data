05700efd31ce1517040bf71eb0330399d216b884
Asif Laldin <laldin.asif@gmail.com>
2025-04-26 23:02:34 +0100

    created sleep api endpoints

---
+++ api/sleep_routes.py
@@ -0,0 +1,17 @@
+from db.database import get_db
+from crud.sleep import get_sleep
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from typing import List
+from schemas.sleep import SleepSchema
+
+router = APIRouter()
+
+@router.get("/sleep/",
+            name="Get all sleeps",
+            description="An endpoint for getting all sleep data",
+            response_model=List[SleepSchema]
+            )
+
+def list_sleep(skip:int=0,limit:int=100,db:Session =Depends(get_db)):
+    return get_sleep(db,skip=skip,limit=limit)
